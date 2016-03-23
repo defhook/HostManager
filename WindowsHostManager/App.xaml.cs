1577883bca2832c70abe823b29197dfb0c309fbf
lniwn <lniwn@live.com>
2016-03-24 01:06:38 +0800

    [feature] run as administrator

--- WindowsHostManager/App.xaml.cs
+++ WindowsHostManager/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Security.Principal;
+using System.Diagnostics;
 
 namespace WindowsHostManager
 {
@@ -13,5 +15,38 @@ namespace WindowsHostManager
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            changePrivilege();
+        }
+
+        private void changePrivilege()
+        {
+            var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool adminMode = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            if(!adminMode)
+            {
+                //var startInfo = new ProcessStartInfo();
+                var startInfo = Process.GetCurrentProcess().StartInfo;
+                startInfo.Verb = "runas";
+                startInfo.FileName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                startInfo.UseShellExecute = true;
+
+                try
+                {
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    MessageBox.Show("请以管理员权限运行程序", "获取管理员权限失败",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    Environment.Exit(-1);
+                }
+            }
+        }
     }
 }
---
+++ WindowsHostManager/Controller/HostReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsHostManager.Controller
+{
+    public class HostReader
+    {
+        private string getHostPath()
+        {
+            string windir = Environment.SystemDirectory;
+            System.IO.Path.Combine(windir, "drivers/etc/hosts");
+            return "";
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint GetWindowsDirectory(StringBuilder buffer, uint usize);
+
+        private string getWindowsDirectory()
+        {
+            uint usize = 0;
+            usize = GetWindowsDirectory(null, usize);
+
+            StringBuilder sb = new StringBuilder((int)usize);
+            GetWindowsDirectory(sb, usize);
+            return sb.ToString();
+        }
+    }
+}
